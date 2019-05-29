d14cf7dddaaf1e33c34befc95b00e09786751d39
Drew Beaudry <drew_beaudry@outlook.com>
2019-05-28 20:49:19 -0500

    Successfully using axios for GET/POST req w/ API Added functionality to get sample TODO objects from http://jsonplaceholder.typicode.com/todos

--- CrashCourse/src/App.vue
+++ CrashCourse/src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -20,33 +21,39 @@ export default {
   },
   data () {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo (id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
+      axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => {
+        this.todos = this.todos.filter(todo => todo.id !== id)
+        return this.todos
+      })
+      .catch(err => console.log(err))
     },
     addTodo (newTodo) {
-      this.todos = [...this.todos, newTodo]
+      const { title, completed } = newTodo
+      axios.post('http://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+      .then(res => {
+        this.todos = [...this.todos, res.data]
+        return this.todos
+      })
+      .catch(err => console.log(err))
     }
-  }
+  },
+  created () {
+      axios.get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => {
+        this.todos = res.data
+        return this.todos
+      })
+      .catch(err => console.log(err))
+    }
 }
 </script>
 
